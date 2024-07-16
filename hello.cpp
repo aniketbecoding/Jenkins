4ae44f38c13b7daf6a23e736d661dd3b2e49ccc1
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2024-07-16 23:48:13 +0530

    Update and rename hello.cpp to hello.py

--- hello.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello I am a c++ code from one of your repositories";
-    return 0;
-}
---
+++ hello.py
@@ -0,0 +1,1 @@
+print("hello, I am from one of your repos")
